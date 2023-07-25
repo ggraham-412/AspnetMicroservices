2a3946e2bd5ee597c287223ba664fcd2e53c0d49
Greg Graham <ggraham412@yahoo.com>
2023-07-24 19:59:36 -0500

    Blah

---
+++ src/Services/Catalog/Catalog.API/Repositories/ProductRepository.cs
@@ -0,0 +1,55 @@
+﻿using Catalog.API.Data;
+using Catalog.API.Entities;
+using MongoDB.Driver;
+
+namespace Catalog.API.Repositories
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly ICatalogContext _context;
+
+        public ProductRepository(ICatalogContext context) 
+        {
+            _context = context ?? throw new ArgumentNullException(nameof(context));
+        }
+
+        public async Task CreateProduct(Product product)
+        {
+            await _context.Products.InsertOneAsync(product);
+        }
+
+        public async Task<bool> DeleteProduct(string id)
+        {
+            var result = await _context.Products.DeleteOneAsync(filter: x => x.Id == id);
+            return result.IsAcknowledged && result.DeletedCount > 0;
+        }
+
+        public async Task<Product> GetProductById(string id)
+        {
+            return await _context.Products.Find(x => x.Id == id).FirstOrDefaultAsync();
+        }
+
+        public async Task<IEnumerable<Product>> GetProducts()
+        {
+            return await _context.Products.Find(x => true).ToListAsync();
+        }
+
+        public async Task<IEnumerable<Product>> GetProductsByCategory(string category)
+        {
+            FilterDefinition<Product> filter = Builders<Product>.Filter.Eq(x => x.Category, category);
+            return await _context.Products.Find(filter).ToListAsync();
+        }
+
+        public async Task<IEnumerable<Product>> GetProductsByName(string name)
+        {
+            FilterDefinition<Product> filter = Builders<Product>.Filter.Eq(x => x.Name, name);
+            return await _context.Products.Find(filter).ToListAsync();
+        }
+
+        public async Task<bool> UpdateProduct(Product product)
+        {
+            var result = await _context.Products.ReplaceOneAsync(filter: g => g.Id == product.Id, replacement: product);
+            return result.IsAcknowledged && result.ModifiedCount > 0;
+        }
+    }
+}
